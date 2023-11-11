bf4006965433f4a4757e2a25d31066b9b2a97b98
douglas-front <douglasjknw007@gmail.com>
2023-11-11 12:27:29 -0300

    remove image required

--- models/Product.js
+++ models/Product.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const ProductSchema = new Schema({
     name: { type: String, required: true },
-    file: { type: String, required: true },
+    file: { type: String, required: false },
 });
 
 module.exports = mongoose.model("Product", ProductSchema);
