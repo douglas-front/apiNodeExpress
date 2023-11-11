cd6c343962b800d7dea61bc3f7232d0bf2b13c14
douglas-front <douglasjknw007@gmail.com>
2023-11-11 11:56:12 -0300

    test

--- config/multer.js
+++ config/multer.js
@@ -1,15 +1,16 @@
-const multer = require("multer");
-const path = require("path");
+const multer = require("multer")
+
+const path = require("path")
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        cb(null, "./images/");
+        cb(null, "images/")
     },
     filename: function(req, file, cb) {
-        cb(null, Date.now() + path.extname(file.originalname));
+        cb(null, Date.now() + path.extname(file.originalname))
     }
-});
+})
 
-const upload = multer({ storage });
+const upload = multer({ storage })
 
-module.exports = upload;
+module.exports = upload
