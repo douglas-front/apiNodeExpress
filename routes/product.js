e46aa4b1ed0db9b53da838354a6dd0ff5b8bddea
douglas-front <douglasjknw007@gmail.com>
2023-11-11 12:05:27 -0300

    /post add

--- routes/product.js
+++ routes/product.js
@@ -7,7 +7,7 @@ const ProductController = require("../controllers/productController")
 
 console.log("/product route has connected")
 
-router.post("/", upload.single("file"), ProductController.create)
+router.post("/post", upload.single("file"), ProductController.create)
 router.get("/", ProductController.findAll)
 router.delete("/:id", ProductController.remove)
 
